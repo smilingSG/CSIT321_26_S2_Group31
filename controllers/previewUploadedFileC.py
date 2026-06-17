8c7106392917e0c5cc860e24307b334b39cd5b89
smilingSG <darrenhow87@gmail.com>
2026-06-18 03:59:51 +0800

    Protect upload preview by session owner

--- controllers/previewUploadedFileC.py
+++ controllers/previewUploadedFileC.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, session, url_for
 
 from entities.File import File
 
@@ -8,7 +8,15 @@ preview_bp = Blueprint("preview_bp", __name__)
 @preview_bp.route("/upload/preview/<int:file_id>", methods=["GET"])
 def generatePreview(file_id: int):
 
-    preview_data = File.getFilePreviewDetails(file_id)
+    owner_id = session.get("user_id")
+
+    if owner_id is None:
+        return redirect(url_for("login_bp.login"))
+
+    preview_data = File.getFilePreviewDetails(
+        file_id,
+        owner_id
+    )
 
     if preview_data is None:
         return "Preview data could not be found.", 404
