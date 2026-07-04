28c6d1d5cf424a91955d2e0b0862256fe9083f29
smilingSG <darrenhow87@gmail.com>
2026-07-05 04:00:18 +0800

    Register reconstruction controller

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ from controllers.storeFragmentC import store_fragment_bp
 from controllers.cancelProcessingC import cancel_processing_bp
 from controllers.fileManagementC import file_management_bp
 from controllers.SearchFileC import search_file_bp
+from controllers.ReconstructFileC import reconstruct_file_bp
 
 app = Flask(__name__)
 app.secret_key = "temporary_secret_key"
@@ -50,6 +51,7 @@ app.register_blueprint(store_fragment_bp)
 app.register_blueprint(cancel_processing_bp)
 app.register_blueprint(file_management_bp)
 app.register_blueprint(search_file_bp)
+app.register_blueprint(reconstruct_file_bp)
 app.register_blueprint(admin_bp)
 app.register_blueprint(admin_search_bp)
 app.register_blueprint(admin_update_bp)
