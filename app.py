ef0d263281c75fee3818d9b489f6b308514008ec
smilingSG <darrenhow87@gmail.com>
2026-07-05 17:11:46 +0800

    Register password policy blueprint

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ from controllers.fileManagementC import file_management_bp
 from controllers.SearchFileC import search_file_bp
 from controllers.ReconstructFileC import reconstruct_file_bp
 from controllers.MaxExpirySettingsC import max_expiry_settings_bp
+from controllers.PasswordPolicyC import password_policy_bp
 
 app = Flask(__name__)
 app.secret_key = "temporary_secret_key"
@@ -54,6 +55,7 @@ app.register_blueprint(file_management_bp)
 app.register_blueprint(search_file_bp)
 app.register_blueprint(reconstruct_file_bp)
 app.register_blueprint(max_expiry_settings_bp)
+app.register_blueprint(password_policy_bp)
 app.register_blueprint(admin_bp)
 app.register_blueprint(admin_search_bp)
 app.register_blueprint(admin_update_bp)
