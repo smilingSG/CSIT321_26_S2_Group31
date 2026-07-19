3f87b2a4616972362c9b9ef2a4f9f35ab71e6d56
soapsz <calvingoh80@gmail.com>
2026-07-19 14:07:46 +0800

    Addition of unsaved changes to fragment configuration

--- controllers/configureFragmentsC.py
+++ controllers/configureFragmentsC.py
@@ -8,7 +8,12 @@ from flask import url_for
 
 # Import entities used to retrieve file details and storage node information.
 from entities.File import File
-from entities.StorageNode import StorageNode
+
+# local
+# from entities.StorageNodeLocal import StorageNode
+
+# OCI
+from entities.StorageNodeOCI import StorageNode
 
 
 # Create the blueprint containing the fragment configuration routes.
